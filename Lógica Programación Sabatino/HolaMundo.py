60f0a69285da584beea39de6c1dc71dcf1ab42d9
sitesystem <l_aloaxa05@hotmail.com>
2022-06-11 11:04:07 -0500

    Hola Mundo 1ra actualización

---
+++ Lógica Programación Sabatino/HolaMundo.py
@@ -0,0 +1,11 @@
+# EJEMPLO 1. SALUDO
+# COMENTARIOS: Sirven para documentar y detallar cada parte del código o programa.
+# Los comentarios no los toma en cuenta Python y no se ejecutan
+
+# Imprimir o mostrar resultados en pantalla de salida
+print("HOLA MUNDO")
+print('Eduardo González Bernal');
+print(10+5)
+print(20-2)
+print(4*7)
+print(9/3)
